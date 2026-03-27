f1aae5c09110d10746b445e2cc86278a18bb583b
Sobhy-Abdelhakam <sobhy.abdelhakam25@gmail.com>
2026-03-27 22:30:46 +0200

    add hilt compose and viewmodel compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     ksp(libs.androidx.room.ksp)
     implementation("com.google.dagger:hilt-android:2.59.2")
     ksp("com.google.dagger:hilt-android-compiler:2.59.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
 }
